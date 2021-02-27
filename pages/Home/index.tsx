2d7eb68f4e5af61d535f3090b26cbfd55cc279d9
didisouzacosta <adrianosouzacostaios@gmail.com>
2021-02-27 20:33:05 -0300

    Organiznado arquivos

---
+++ pages/Home/index.tsx
@@ -0,0 +1,84 @@
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+
+import styles from "./styles.module.css";
+
+const Home = () => {
+  const name = "Adriano Souza Costa";
+  const title = "Adriano | iOS and React Native Developer";
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Learn how to build a personal website using Next.js"
+        />
+        <meta
+          property="og:image"
+          content={`https://og-image.vercel.app/${encodeURI(
+            title
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
+        <meta name="og:title" content={title} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <header className={styles.header}>
+        <Image
+          priority
+          src="/images/profile.png"
+          className={styles.borderCircle}
+          height={144}
+          width={144}
+          alt={name}
+        />
+        <h1 className={styles.heading}>{name}</h1>
+        <p className={styles.subHeading}>Desenvolvedor iOS e React Native</p>
+      </header>
+      <section className={styles.infos}>
+        <p>
+          Além de um cara legal, trabalho criando soluções mobile utilizando
+          Swift e React Native (também tenho um dedido no design rsrs). E levo a
+          vida devagar curtindo muito 🐶 🎮 🍿 ☕
+        </p>
+        <Link href="https://dev.to/didisouzacosta" replace={false}>
+          <a target="_blank" rel="noreferrer">
+            <Image
+              priority
+              src="/icons/devto_icon.png"
+              height={44}
+              width={44}
+              alt="Dev.to"
+            />
+          </a>
+        </Link>
+        <Link href="https://github.com/didisouzacosta">
+          <a target="_blank" rel="noreferrer">
+            <Image
+              priority
+              src="/icons/github_icon.png"
+              height={44}
+              width={44}
+              alt="Github"
+            />
+          </a>
+        </Link>
+        <Link href="https://www.linkedin.com/in/adrianosouzacosta/">
+          <a target="_blank" rel="noreferrer">
+            <Image
+              priority
+              src="/icons/linkedin_icon.png"
+              height={44}
+              width={44}
+              alt="Linkedin"
+            />
+          </a>
+        </Link>
+      </section>
+    </div>
+  );
+};
+
+export default Home;
---
+++ pages/_document.tsx
@@ -0,0 +1,38 @@
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="pt-br">
+        <Head>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PFN7C5M');`,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
