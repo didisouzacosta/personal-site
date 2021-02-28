47b0fe27bb617f4cdb464dd4058675f1e6851ddd
didisouzacosta <adrianosouzacostaios@gmail.com>
2021-02-27 21:09:59 -0300

    Melhora centralização

--- pages/Home/index.tsx
+++ pages/Home/index.tsx
@@ -1,13 +1,31 @@
 import Image from "next/image";
 import Link from "next/link";
+import Head from "next/head";
 
 import styles from "./styles.module.css";
 
-const Home = () => {
-  const name = "Adriano Souza Costa";
+const name = "Adriano Souza Costa";
+const title = "Adriano | iOS and React Native Developer";
 
+const Home = () => {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>{title}</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Learn how to build a personal website using Next.js"
+        />
+        <meta
+          property="og:image"
+          content={`https://og-image.vercel.app/${encodeURI(
+            title
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
+        <meta name="og:title" content={title} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
       <header className={styles.header}>
         <Image
           priority
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,24 +13,9 @@ class MyDocument extends Document {
   }
 
   render() {
-    const title = "Adriano | iOS and React Native Developer";
-
     return (
       <Html lang="pt-br">
         <Head>
-          <link rel="icon" href="/favicon.ico" />
-          <meta
-            name="description"
-            content="Learn how to build a personal website using Next.js"
-          />
-          <meta
-            property="og:image"
-            content={`https://og-image.vercel.app/${encodeURI(
-              title
-            )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-          />
-          <meta name="og:title" content={title} />
-          <meta name="twitter:card" content="summary_large_image" />
           <script
             async
             src="https://www.googletagmanager.com/gtag/js?id=GTM-PFN7C5M"
