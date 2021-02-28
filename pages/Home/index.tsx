7cb24b149fe5673a2e334b7a1468898f49972b2e
didisouzacosta <adrianosouzacostaios@gmail.com>
2021-02-27 21:37:38 -0300

    Corrigindo GTM

--- pages/Home/index.tsx
+++ pages/Home/index.tsx
@@ -17,12 +17,7 @@ const Home = () => {
           name="description"
           content="Learn how to build a personal website using Next.js"
         />
-        <meta
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            title
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
+        <meta property="og:image" content="/images/profile.png" />
         <meta name="og:title" content={title} />
         <meta name="twitter:card" content="/images/profile.png" />
       </Head>
