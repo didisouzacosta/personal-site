b89ef5d2539d140bbeec6a157c90c41721a709b9
Adriano Souza Costa <adrianosouzacostaios@gmail.com>
2025-03-27 18:00:29 -0300

    Fix urls

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
 
 export default defineConfig({
-  site: 'https://didisouzacosta.github.io',
-  base: '/PersonalWebSite',
-  output: 'static',
+  // site: 'https://didisouzacosta.github.io',
+  // base: '/PersonalWebSite',
+  // output: 'static',
   integrations: [icon()]
 });
