e856bae2af68a8e5c54e73e9b57de7685651619e
Adriano Souza Costa <adrianosouzacostaios@gmail.com>
2025-03-28 11:12:36 -0300

    Fix github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
 
 export default defineConfig({
+  site: 'https://adrianosouzacosta.com.br',
   integrations: [icon()]
 });
